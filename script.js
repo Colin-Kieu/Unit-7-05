31678efce08546dbbdc71fb22e873919330e123b
ColinKieu <colin.kieu@ocsbstudent.ca>
2021-03-25 17:39:35 +0000

    linter

--- script.js
+++ script.js
@@ -15,7 +15,5 @@ function action () {
   } else if ((age >= 18) && (day === 'saturday' || day === 'sunday')) {
     document.getElementById('answer').innerHTML = 'Relax, its the weekend!'
   } else if ((age >= 18) && (day !== 'saturday' || day !== 'sunday')) {
-    document.getElementById('answer').innerHTML = 'Time for Work!'
-  }
-
+    document.getElementById('answer').innerHTML = 'Time for Work!'}
 }
